ae3a3dc49a7adfe8f90dc94108f68e53ed0ed5a9
Petar Velikov <pepo_velikov@mail.bg>
2024-07-31 16:19:02 +0300

    Partner routes

---
+++ routes/partnerRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Partner = require('../models/partner');
+const auth = require('../middleware/auth');
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const partners = await Partner.find();
+    res.json(partners);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/', auth, async (req, res) => {
+  const partner = new Partner({
+    name: req.body.name,
+    bulstat: req.body.bulstat,
+    addres: req.body.addres,
+    phone: req.body.phone,
+    email: req.body.email
+  });
+  try {
+    const newPartner = await partner.save();
+    res.status(201).json(newPartner);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
