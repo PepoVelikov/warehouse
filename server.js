c3cf4ea975877625e00116eda0ae1fa2d6e8eaba
Petar Velikov <pepo_velikov@mail.bg>
2024-08-07 15:15:48 +0300

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/items', require('./routes/itemRoutes'));
