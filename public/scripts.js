499e23dd2fadbda34e6a04d6dc565ef5b76fe980
Petar Velikov <pepo_velikov@mail.bg>
2024-08-08 16:21:16 +0300

    update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   if (loginForm) {
-    loginForm.addEventListener('submit', (e) => {
+    loginForm.addEventListener('submit', async (e) => {
       e.preventDefault();
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
