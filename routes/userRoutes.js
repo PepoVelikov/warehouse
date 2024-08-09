4bdcbaff02630f4ce796e506a4a7b24d2e8155a4
Petar Velikov <pepo_velikov@mail.bg>
2024-08-09 14:57:36 +0300

    update userRoutes for reg users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,22 +6,25 @@ const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   const { name, password, email } = req.body;
+  console.log('Received registration request:', { name, password, email });
   try {
     let user = await User.findOne({ name });
     if (user) {
+      console.log('User already exists');
       return res.status(400).json({ message: 'User already exists' });
     }
-    User = new User({ name, password, email });
+    user = new User({ name, password, email });
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
     await user.save();
+    console.log('User registered successfully:', user);
     const payload = { user: { id: user.id }};
     jwt.sign(payload, 'yourJWTSecret', { expireIn: 360000 }, (err, token) => {
       if (err) throw err;
       res.json({ token });
     });
   } catch (error) {
-    console.error(err.message);
+    console.error('Error registering user:', error.message);
     res.status(500).send('Server error');
   }
 });
