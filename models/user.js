26ffc073528da1d883bb5ef0b259efff340adff9
Petar Velikov <pepo_velikov@mail.bg>
2024-08-09 15:39:14 +0300

    update user.js with unique: true for name

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, require: true},
+  name: { type: String, require: true, unique: true},
   email: { type: String, require: true, unique: true},
   password: { type: String, require: true},
   date: { type: Date, default: Date.now}
