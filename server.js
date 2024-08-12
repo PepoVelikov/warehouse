e889842d9e215e7273cb6ce035aeaf23cdd5823b
Petar Velikov <pepo_velikov@mail.bg>
2024-08-12 16:36:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
+app.use('/api/dashboard', (req, res) => {
+  res.json({ message : 'Welcome to the Dashboard' })
+});
+
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/items', require('./routes/itemRoutes'));
 app.use('/api/partner', require('./routes/partnerRoutes'));
