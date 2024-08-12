ccc0b792bc5ec82062e68c49a408a1a16918d3cd
Petar Velikov <pepo_velikov@mail.bg>
2024-08-12 16:39:15 +0300

    Update item.js

--- models/item.js
+++ models/item.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ItemSchema = new mongoose.Schema({
   name: { type: String, require: true},
   unit: { type: String, require: true},
+  quantity: { type: Number, require: true},
   price: { type: Number, require: true}
 });
 
